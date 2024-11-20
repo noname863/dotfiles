1fafc8ab7977449fae4740faaef432965b6103de
Pavel Amelkov <stelss78@gmail.com>
2024-11-20 16:42:07 +0200

    Removed word wrap in neovim

--- neovim/.config/nvim/lua/config.lua
+++ neovim/.config/nvim/lua/config.lua
@@ -5,11 +5,11 @@ vim.g.netrw_bufsettings="noma nomod nu nobl nowrap ro rnu"
 
 -- line limit
 vim.o.colorcolumn = "80"
-
 -- tabs
 vim.o.tabstop=4
 vim.o.softtabstop=4
 vim.o.shiftwidth=4
 vim.o.expandtab=true
 
+vim.o.wrap=false
 
